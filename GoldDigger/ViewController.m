133468070eb96766f0e9c77b49ac300b51302a3b
Alvin Du <alvindu@gmail.com>
2014-12-11 20:36:33 -0800

    Initial Commit

---
+++ GoldDigger/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GoldDigger
+//
+//  Created by Alvin Du on 12/11/14.
+//  Copyright (c) 2014 Alvin Du. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
